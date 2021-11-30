ef30b886bd5339182f4675b469524d41ac13dc79
Maicon Pessoa <86012549+pessoamaicon@users.noreply.github.com>
2021-11-30 11:27:17 -0300

    Delete Exec8.py

--- Exec8.py
+++
@@ -1,3 +0,0 @@
-valorHora = float(input("Informe o valor de quanto você ganha por hora:"))
-horasTrabalhadas = float(input("Informe quantas horas você trabalhou no mês:"))
-print("O seu salário é R$:", valorHora*horasTrabalhadas)
